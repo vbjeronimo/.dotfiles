0f02c5aeda1756e55cff04706144b4d9a67b202f
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-02-04 13:41:31 -0400

    Added cursorline, relativenumber, and indentation options

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -283,6 +283,13 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.o.relativenumber = true
+
+vim.o.cursorline = true
+
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
