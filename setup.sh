e53d446f2302624ec7e1296b610975fd2056a691
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-02-04 12:34:18 -0400

    Removed some stuff from the setup script (using Endeavour OS again)

--- setup.sh
+++ setup.sh
@@ -2,38 +2,6 @@
 
 set -euo pipefail
 
-DOTFILES_REPO=https://github.com/vbjeronimo/.dotfiles.git
-
-setup_pacman() {
-    echo "[*] Starting pacman setup..."
-
-    sudo sed -i 's/#Color/Color/' /etc/pacman.conf
-    sudo sed -i 's/#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-    sudo sed -i 's/#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
-
-    echo "[*] Pacman setup complete!"
-}
-
-setup_base_system() {
-    echo "[*] Starting base system setup..."
-
-    sudo pacman -S --noconfirm --needed \
-        base-devel \
-        man-db \
-        man-pages \
-        texinfo
-
-    if lscpu | grep "Vendor ID" | grep -q "AMD"; then
-        echo "[**] Installing AMD microcode..."
-        sudo pacman -S --noconfirm --needed amd-ucode
-    elif lscpu | grep "Vendor ID" | grep -q "Intel"; then
-        echo "[**] Installing Intel microcode..."
-        sudo pacman -S --noconfirm --needed intel-ucode
-    fi
-
-    echo "[*] Base system setup complete!"
-}
-
 setup_desktop_environment() {
     echo "[*] Starting desktop environment setup..."
 
@@ -43,14 +11,9 @@ setup_desktop_environment() {
         feh \
         firefox \
         i3-wm \
-        nvidia \
         obsidian \
         papirus-icon-theme \
-        polybar \
         rofi \
-        xorg-server \
-        xorg-xinit \
-        xorg-xrandr
 
     echo "[*] Desktop environment setup complete!"
 }
@@ -65,8 +28,6 @@ setup_shell() {
         pass
         stow
         tmux
-        unzip
-        wget
         xclip
     )
     local fonts=(
@@ -114,32 +75,6 @@ setup_syncthing() {
     echo "[**] Syncthing setup complete!"
 }
 
-setup_dotfiles() {
-    local dotfiles_dir="$HOME"/.dotfiles
-
-    echo "[*] Setting up dotfiles dir..."
-
-    if ! [[ -d "$dotfiles_dir" ]];then
-        echo "[**] Dotfiles dir not found. Cloning it..."
-
-        git clone "$DOTFILES_REPO" "$dotfiles_dir"
-        cd "$dotfiles_dir" || exit
-
-        # this is a clever trick to force stow to create the symlinks that we need here
-        # and then restoring the files to their original content once the symlinks exist
-        stow --adopt */
-        git restore .
-    else
-        echo "[**] Dotfiles already cloned. Restowing..."
-        cd "$dotfiles_dir" || exit
-        stow --restow */
-    fi
-
-    cd - > /dev/null || exit
-
-    echo "[*] Dotfiles dir setup complete!"
-}
-
 create_home_dirs() {
     echo "[*] Creating home directories..."
 
@@ -161,16 +96,10 @@ create_home_dirs() {
 echo "[*] Enabling sshd.service..."
 sudo systemctl enable --now sshd.service
 
-setup_pacman
-
 sudo pacman -Syu --noconfirm
 
-setup_base_system
 setup_shell
 setup_dev_environment
 setup_desktop_environment
 setup_syncthing
-setup_dotfiles
 create_home_dirs
-
-# TODO: find a better way to setup the xbdkeymaps (it goes back to us after an update)
