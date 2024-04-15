a644950045d7418aea2f190ec0a709fcdc5b371a
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-04-15 20:08:26 -0300

    Wrote script to build Neovim from source

--- setup/components/30-terminal/10-nvim.sh
+++ setup/components/30-terminal/10-nvim.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
-set -e
+set -eu
+
+source "${ENGI_DIR}/options.env"
+
+echo "[*] Running Neovim setup script"
+
+if command -v nvim &> /dev/null; then
+    echo "[*] Nothing to do"
+    exit 0
+fi
 
 echo "[*] Installing Neovim from source"
+
+sudo apt-get install -y --no-upgrade \
+    build-essential \
+    cmake \
+    curl \
+    gettext \
+    ninja-build \
+    unzip
+
+sudo git clone https://github.com/neovim/neovim /opt/neovim
+(
+    cd /opt/neovim
+    git checkout "$NEOVIM_VERSION"
+    sudo make CMAKE_BUILD_TYPE=RelWithDebInfo
+    sudo make installcd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+)
+
+echo "[*] Installing Telescope dependencies"
+sudo apt-get install -y --no-upgrade \
+    fd-find \
+    ripgrep
