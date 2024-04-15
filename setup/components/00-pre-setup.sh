005bf87f4cf1a9c4395af70ec4cccb5412be9337
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-04-15 17:24:39 -0300

    (WIP) Working on a prototype for a setup runner script
    Got 'list', 'enable', and 'disable' commands working

--- setup/components/00-pre-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "[*] Updating apt cache"
-sudo apt-get update
--- setup/main.sh
+++ setup/main.sh
@@ -2,20 +2,68 @@
 
 set -e
 
-SETUP_DIR="$(readlink -f "$(dirname "$0")")"
+BASE_DIR="/home/vitor/wip-dotfiles/setup/components"
 
-TEST_ARG=""
+DEFAULT_MOD="755"
 
-for opt in "$@"; do
-    case "$opt" in
-        -t | --test)
-            TEST_ARG="--test"
-            ;;
-    esac
-done
+if [[ "$#" -eq 0 ]]; then
+    echo "usage: ..."
+    exit 1
+fi
 
-echo "[*] Starting main setup script"
-run-parts \
-    --exit-on-error \
-    --regex '.*' \
-    "${SETUP_DIR}/components"
+command="$1"
+shift
+
+case "$command" in
+    setup)
+        echo "setup $@"
+        ;;
+    list)
+        find "${BASE_DIR}" -type d -exec run-parts --test --regex '.*' {} \; \
+            | tree --fromfile
+        ;;
+    enable)
+        IFS=: read -r component target <<< "$1"
+        IFS=, read -ra scripts <<< "$target"
+
+        if [[ "${#scripts[@]}" -eq 0 ]]; then
+            echo "Enabling all scripts in $component"
+            chmod "$DEFAULT_MOD" "${BASE_DIR}"/*"${component}"/*.sh
+        fi
+
+        for script in "${scripts[@]}"; do
+            echo "Enabling script '$script' in $component"
+            chmod "$DEFAULT_MOD" "${BASE_DIR}"/*"${component}"/*${script}.sh
+        done
+        ;;
+    disable)
+        IFS=: read -r component target <<< "$1"
+        IFS=, read -ra scripts <<< "$target"
+
+        if [[ "${#scripts[@]}" -eq 0 ]]; then
+            echo "Disabling all scripts in $component"
+            chmod 644 "${BASE_DIR}"/*"${component}"/*.sh
+        fi
+
+        for script in "${scripts[@]}"; do
+            echo "Disabling script '$script' in $component"
+            chmod 644 "${BASE_DIR}"/*"${component}"/*${script}.sh
+        done
+        ;;
+    new)
+        echo "new $@"
+        ;;
+    edit)
+        echo "edit $@"
+        ;;
+    delete)
+        echo "delete $@"
+        ;;
+    show)
+        echo "show $@"
+        ;;
+    *)
+        echo "Error: unknown command: $command"
+        exit 1
+        ;;
+esac
