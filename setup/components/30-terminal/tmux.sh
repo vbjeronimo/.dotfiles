2f2203a8f4c4d7209d21fa8c9c8c388b3ee0c089
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-04-27 08:49:19 -0300

    Added TPM installation to the tmux script

--- setup/components/30-terminal/tmux.sh
+++ setup/components/30-terminal/tmux.sh
@@ -6,39 +6,45 @@ source "${ENGI_DIR}/options.env"
 
 echo "[*] Running tmux install script"
 
-if command -v tmux &> /dev/null; then
-    echo "[*] Nothing to do"
-    exit 0
+if ! command -v tmux &> /dev/null; then
+    echo "[*] Installing tmux from source (version ${TMUX_VERSION})"
+
+    echo "[*] Installing run dependencies"
+    sudo apt-get install -y --no-upgrade \
+        libevent \
+        ncurses || true
+
+    echo "[*] Installing build dependencies"
+    sudo apt-get install -y --no-upgrade \
+        bison \
+        build-essential \
+        libevent-dev \
+        ncurses-dev \
+        pkg-config
+
+    if [[ -d /opt/tmux-* ]]; then
+        echo "[*] Removing previous tmux tarballs"
+        sudo rm -rv /opt/tmux-*
+    fi
+
+    echo "[*] Pulling tarball and building tmux"
+    curl -L "https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz" \
+        | sudo tar xz -C /opt
+
+    (
+        cd /opt/tmux-*
+        sudo ./configure
+        sudo make &> /dev/null && sudo make install &> /dev/null
+    )
+
+    echo "[*] Tmux installed!"
+else
+    echo "[*] Tmux is already installed. Nothing to do"
 fi
 
-echo "[*] Installing tmux from source (version ${TMUX_VERSION})"
-
-echo "[*] Installing run dependencies"
-sudo apt-get install -y --no-upgrade \
-    libevent \
-    ncurses || true
-
-echo "[*] Installing build dependencies"
-sudo apt-get install -y --no-upgrade \
-    bison \
-    build-essential \
-    libevent-dev \
-    ncurses-dev \
-    pkg-config
-
-if [[ -d /opt/tmux-* ]]; then
-    echo "[*] Removing previous tmux tarballs"
-    sudo rm -rv /opt/tmux-*
+if [[ ! -e ~/.tmux/plugins/tpm ]]; then
+    echo "[*] Installing Tmux Package Manager"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+    echo "[*] TPM is already installed. Nothing to do"
 fi
-
-echo "[*] Pulling tarball and building tmux"
-curl -L "https://github.com/tmux/tmux/releases/download/${TMUX_VERSION}/tmux-${TMUX_VERSION}.tar.gz" \
-    | sudo tar xz -C /opt
-
-(
-    cd /opt/tmux-*
-    sudo ./configure
-    sudo make &> /dev/null && sudo make install &> /dev/null
-)
-
-echo "[*] Tmux installed!"
