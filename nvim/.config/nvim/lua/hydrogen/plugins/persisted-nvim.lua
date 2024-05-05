8cdc1c989a5900ff5dd7b300b0a39895a6435f55
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-05-05 14:43:20 -0300

    Disabled 'persisted.nvim'

--- nvim/.config/nvim/lua/hydrogen/plugins/persisted-nvim.lua
+++ nvim/.config/nvim/lua/hydrogen/plugins/persisted-nvim.lua
@@ -1,6 +1,7 @@
 return {
   "olimorris/persisted.nvim",
   lazy = false, -- make sure the plugin is always loaded at startup
+  enabled = false,
   opts = {
       autosave = true,
       autoload = true,
