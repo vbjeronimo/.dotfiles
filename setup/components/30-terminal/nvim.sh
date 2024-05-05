a4c6ef3ca1d0eeee77de7859cd0990a5f4a8fcd0
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-05-05 11:36:56 -0300

    Improved the Neovim setup script

--- setup/components/30-terminal/nvim.sh
+++ setup/components/30-terminal/nvim.sh
@@ -6,30 +6,32 @@ source "${ENGI_DIR}/options.env"
 
 echo "[*] Running Neovim setup script"
 
-if command -v nvim &> /dev/null; then
-    echo "[*] Nothing to do"
-    exit 0
+if ! command -v nvim &> /dev/null; then
+    echo "[*] Installing build dependencies"
+    sudo apt-get install -y --no-upgrade \
+        build-essential \
+        cmake \
+        curl \
+        gettext \
+        ninja-build \
+        unzip
+
+    if [[ ! -d /opt/neovim ]]; then
+        echo "[*] Cloning Neovim into /opt/neovim (version $NEOVIM_VERSION)"
+        sudo git clone https://github.com/neovim/neovim /opt/neovim
+    fi
+
+    echo "[*] Building Neovim from source"
+    (
+        sudo chown "$USER:$USER" -R /opt/neovim
+        cd /opt/neovim
+        git checkout "$NEOVIM_VERSION"
+        make CMAKE_BUILD_TYPE=RelWithDebInfo
+        cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
+    )
+else
+    echo "[*] Neovim already installed. Nothing to do"
 fi
-
-echo "[*] Installing Neovim from source"
-
-echo "[*] Installing build dependencies"
-sudo apt-get install -y --no-upgrade \
-    build-essential \
-    cmake \
-    curl \
-    gettext \
-    ninja-build \
-    unzip
-
-echo "[*] Cloning and building Neovim (version $NEOVIM_VERSION)"
-sudo git clone https://github.com/neovim/neovim /opt/neovim
-(
-    cd /opt/neovim
-    git checkout "$NEOVIM_VERSION"
-    sudo make CMAKE_BUILD_TYPE=RelWithDebInfo
-    sudo make installcd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
-)
 
 echo "[*] Installing Telescope dependencies"
 sudo apt-get install -y --no-upgrade \
