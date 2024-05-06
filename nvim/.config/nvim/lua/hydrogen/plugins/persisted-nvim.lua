aa9a238355c9f2d085dd4d2bd6c578335be98383
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-05-06 19:12:48 -0300

    Removed the 'persisted.nvim' plugin

--- nvim/.config/nvim/lua/hydrogen/plugins/persisted-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "olimorris/persisted.nvim",
-  lazy = false, -- make sure the plugin is always loaded at startup
-  enabled = false,
-  opts = {
-      autosave = true,
-      autoload = true,
-  }
-}
