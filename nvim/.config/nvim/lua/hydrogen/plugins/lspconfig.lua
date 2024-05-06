8f2da640de14de4e1a46bccbbdf7e82b3c7751fe
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-05-06 19:13:13 -0300

    (WIP) Adding mason-lspconfig

---
+++ nvim/.config/nvim/lua/hydrogen/plugins/lspconfig.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "williamboman/mason-lspconfig.nvim",
+        cmd = "Mason",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup()
+
+            -- After setting up mason-lspconfig you may set up servers via lspconfig
+            -- require("lspconfig").lua_ls.setup {}
+            -- require("lspconfig").rust_analyzer.setup {}
+            -- ...
+        end
+    }
+}
