f8358c68434bf006136926165139dda3a5868929
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-05-18 12:35:31 -0300

    Added code to install and setup pass + gpg keys

--- setup/components/50-applications.sh
+++ setup/components/50-applications.sh
@@ -2,13 +2,31 @@
 
 set -e
 
+source "${ENGI_DIR}/secret.env"
+
 echo "[*] Starting script to install and setup applications"
 
-if ! command syncthing &> /dev/null; then
+if ! command -v syncthing &> /dev/null; then
     echo "[*] Installing and setting up syncthing"
     sudo apt-get install -y --no-upgrade \
         syncthing \
         syncthingtray
 
     systemctl --user enable --now syncthing.service
+fi
+
+if ! command -v pass &> /dev/null; then
+    echo "[*] Installing and setting up pass"
+    sudo apt-get install -y --no-upgrade \
+        git \
+        gnupg \
+        pass
+
+    echo "[*] Cloning the password store repo to $PASSWORD_STORE_DIR"
+    git clone "$PASSWORD_STORE_URL" "$PASSWORD_STORE_DIR"
+
+    echo "[*] Importing GPG keys from $GPG_KEYS_DIR"
+    gpg --import --batch --yes "$GPG_KEYS_DIR"/public.gpg
+    gpg --import --batch --yes --passphrase "$GPG_KEY_PASSWORD" "$GPG_KEYS_DIR"/private.gpg
+    gpg --import-ownertrust --batch --yes "$GPG_KEYS_DIR"/trust.gpg
 fi
