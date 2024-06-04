2a615ad65cc526e3a236aa2bc5fcdfbd0fb5fb9f
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-06-04 14:41:50 -0300

    Added 'spotify' to the list of application packages to install

--- setup/components/50-applications.sh
+++ setup/components/50-applications.sh
@@ -30,3 +30,14 @@ if ! command -v pass &> /dev/null; then
     gpg --import --batch --yes --passphrase "$GPG_KEY_PASSWORD" "$GPG_KEYS_DIR"/private.gpg
     gpg --import-ownertrust --batch --yes "$GPG_KEYS_DIR"/trust.gpg
 fi
+
+if ! command -v spotify &> /dev/null; then
+    echo "[*] Installing and setting up Spotify"
+    sudo mkdir -p /etc/apt/keyrings
+    curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+    echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+    sudo apt update && sudo apt install spotify-client
+
+    # The spotify package is currently broken and installs files owned by uid 1000, fix it
+    dpkg -L spotify-client | sudo xargs chown --no-dereference root:root
+fi
