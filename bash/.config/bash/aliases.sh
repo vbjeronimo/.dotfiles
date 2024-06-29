28bb3168c4d4eabcf91df8d0823222b35fd249bf
Vitor Jeronimo <vitor.bently@hotmail.com>
2024-06-29 12:55:01 -0300

    Removed the bash aliases that I wasn't using

--- bash/.config/bash/aliases.sh
+++ bash/.config/bash/aliases.sh
@@ -5,13 +5,9 @@ alias "cdr"='cd $(git rev-parse --show-toplevel)'
 alias l="exa"
 alias ls="exa"
 alias ll="exa --long --all --icons --git --group-directories-first"
-alias lf="exa --long --all --icons --git --only-files"
-alias ld="exa --long --all --icons --git --only-dirs" 
 
 alias t="exa --tree --icons"
 alias tt="exa --tree --all --icons"
-alias tf="exa --tree --all --icons --only-files"
-alias td="exa --tree --all --icons --only-dirs"
 
 alias ".."="cd .."
 alias sb="cd $SECOND_BRAIN && nvim"
