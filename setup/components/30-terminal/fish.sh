1c261bee02acc40a2f04bc17102cf0fb5edddca0
Vitor Jeronimo <vitor.jeronimo@proton.me>
2024-10-23 21:42:50 -0300

    Added a script to install and setup fish as the default shell

---
+++ setup/components/30-terminal/fish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+source "${ENGI_DIR}/lib/pkg.sh"
+
+non_root_user=$USER
+
+echo "[*] Installing the fish shell"
+pkg_install \
+    fish
+
+if ! echo "$SHELL" | grep -q "fish"; then
+    echo "[*] Setting up fish as the default shell"
+    sudo chsh -s $(which fish) $non_root_user
+else
+    echo "[*] Fish is already set as the default shell. Skipping shell setup"
+fi
