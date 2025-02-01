5ce60afbb12f155a01ed3afad25e46c8345559d3
Vitor Jeronimo <vitor.jeronimo@proton.me>
2025-02-01 17:18:10 -0400

    Some minor changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,5 @@ set -U fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+pyenv init - fish | source
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,7 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
 vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
 
 vim.opt.number = true
 vim.opt.relativenumber = true
