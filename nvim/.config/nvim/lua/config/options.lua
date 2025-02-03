7a86dff329c6e7af7d886028003dc1adabd5cf67
Vitor Jeronimo <vitor.jeronimo@proton.me>
2025-02-03 18:52:26 -0400

    Disabled line wrap and added Teej's source/execution keymaps

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,8 +5,14 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 
+vim.opt.wrap = false
+
 vim.opt.scrolloff = 10
 vim.opt.sidescrolloff = 10
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
