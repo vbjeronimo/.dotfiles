6b12ef44ee23795297ade0e9e3a4e2cb8c605abe
Vitor Jeronimo <vitor.jeronimo@proton.me>
2025-02-04 20:53:11 -0400

    Removed comment

--- nvim/.config/nvim/lua/config/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/config/plugins/colorschemes.lua
@@ -2,7 +2,6 @@ return {
     {
         'marko-cerovac/material.nvim',
         config = function()
-            --Lua:
             vim.g.material_style = "deep ocean"
             vim.cmd("colorscheme material")
         end
